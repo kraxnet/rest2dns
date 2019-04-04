6c9c639f5c59f40813e14ed9242930ed847bdfdd
Ondrej Bima <ondra@bimovi.cz>
2019-04-04 15:00:10 +0200

    conf bere params z ENV

--- config/config.rb
+++ config/config.rb
@@ -8,13 +8,16 @@ module CONF
   DNS_SERVER_TYPE = :knot
   # DNS_SERVER_TYPE = :bind
 
+  # 1 = master. 0 = slave
+  DNS_SERVER_MASTER = (ENV['DNS_MASTER_SERVER'] || 1).to_s == "1"
+
   module KNOT
-    ZONE_FILE_DIR = 'zones/knot'
-    ZONE_FILE_SUFFIX = '.zone'
-    ZONE_FILE_LIST = 'zones/knot-zones.conf'
-    DOMAIN_TEMPLATE_NAME = 'rest2dns'
-    COMMAND_RELOAD = 'knotc reload'
-    COMMAND_CHECKZONE = 'kzonecheck'
+    ZONE_FILE_DIR = ENV['KNOT_ZONE_FILE_DIR'] || '/etc/knot/zones'
+    ZONE_FILE_SUFFIX = ENV['KNOT_ZONE_FILE_SUFFIX'] || '.zone'
+    ZONE_FILE_LIST = ENV['KNOT_ZONE_FILE_LIST'] || '/etc/knot/knot-zones.conf'
+    DOMAIN_TEMPLATE_NAME =  ENV['DOMAIN_TEMPLATE_NAME'] || 'rest2dns'
+    COMMAND_RELOAD =  ENV['KNOT_COMMAND_RELOAD'] || 'knotc reload'
+    COMMAND_CHECKZONE = ENV['KNOT_COMMAND_CHECKZONE'] || 'kzonecheck'
   end
 end
 
